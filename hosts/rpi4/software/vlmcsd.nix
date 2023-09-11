da0336891ca4b95a1ad4546abdb8bb06e4abe618
Cryolitia <Cryolitia@gmail.com>
2023-09-11 14:01:23 +0800

    20230911

--- hosts/rpi4/software/vlmcsd.nix
+++ hosts/rpi4/software/vlmcsd.nix
@@ -2,16 +2,16 @@
 
 {
   environment.systemPackages = with pkgs; [
-    nur.repos.pborzenkov.vlmcsd
+    config.nur.repos.pborzenkov.vlmcsd
   ];
   
   systemd.services.vlmcsd = {
       after = [ "network.target" ];
       description = "vlmcsd";
       wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.nur.repos.pborzenkov.vlmcsd ];
+      path = [ config.nur.repos.pborzenkov.vlmcsd ];
       serviceConfig = {
-        ExecStart = "${pkgs.nur.repos.pborzenkov.vlmcsd}/bin/vlmcsd -D -v";
+        ExecStart = "${config.nur.repos.pborzenkov.vlmcsd}/bin/vlmcsd -D -v";
         DynamicUser = true;
       };
   };
