42db7bf87a37612c4ac9b68637d8598f0f93c3a9
北雁 Cryolitia <Cryolitia@gmail.com>
2023-12-07 07:07:36 +0800

    Prevent HP laptop to toggle airplane mode when lip close.
    https://askubuntu.com/questions/965595/why-does-airplane-mode-keep-toggling-on-my-hp-laptop-in-ubuntu-18-04/965596#965596

--- hosts/laptop/software/default.nix
+++ hosts/laptop/software/default.nix
@@ -3,29 +3,29 @@
 {
   imports =
     [
-        ./syncthing.nix
-        ../../../graphic/software/waydroid.nix
-        ../../../graphic/software
-        ./develop
-        # ./hadoop.nix
+      ./syncthing.nix
+      ../../../graphic/software/waydroid.nix
+      ../../../graphic/software
+      ./develop
+      # ./hadoop.nix
     ];
 
-    environment.systemPackages = with pkgs; [
-        firefox
-        config.nur.repos.xddxdd.wechat-uos
-        texlive.combined.scheme-full
-        jetbrains.idea-ultimate
-        android-tools
-        jetbrains.pycharm-professional
-        #nur-cryolitia.MaaX
-        # config.nur.repos.Freed-Wu.netease-cloud-music
-        wpsoffice
-        config.nur.repos.linyinfeng.wemeet
-        obs-studio
-        androidStudioPackages.beta
-        jetbrains.clion
-        libsForQt5.kleopatra
-        keepassxc
+  environment.systemPackages = with pkgs; [
+    firefox
+    config.nur.repos.xddxdd.wechat-uos
+    texlive.combined.scheme-full
+    jetbrains.idea-ultimate
+    android-tools
+    jetbrains.pycharm-professional
+    #nur-cryolitia.MaaX
+    # config.nur.repos.Freed-Wu.netease-cloud-music
+    wpsoffice
+    config.nur.repos.linyinfeng.wemeet
+    obs-studio
+    androidStudioPackages.beta
+    jetbrains.clion
+    libsForQt5.kleopatra
+    keepassxc
   ];
 
   boot.extraModulePackages = with config.boot.kernelPackages; [
@@ -37,4 +37,18 @@
     "electron-19.0.7"
     "electron-19.1.9"
   ];
+
+  systemd.services.HPLidKeyCode = {
+    description = "Prevent HP laptop to toggle airplane mode when lip close.";
+    serviceConfig = {
+      Type = "oneshot";
+      Restart = "no";
+      ExecStart = "${pkgs.kbd}/bin/setkeycodes e057 240 e058 240";
+    };
+    wantedBy = [
+      "rescue.target"
+      "multi-user.target"
+      "graphical.target"
+    ];
+  };
 }
