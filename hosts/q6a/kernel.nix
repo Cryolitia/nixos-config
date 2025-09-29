561d857ebabb2ef2774fd1edc8cc97a787455b0b
Cryolitia PukNgae <Cryolitia@gmail.com>
2025-09-30 07:01:41 +0800

    q6a

--- hosts/q6a/kernel.nix
+++ hosts/q6a/kernel.nix
@@ -1,8 +1,26 @@
-{ pkgs, inputs, ... }:
+{
+  pkgs,
+  inputs,
+  lib,
+  ...
+}:
 
 pkgs.buildLinux {
-  defconf = "qcom_module_defconfig";
+  defconfig = "qcom_module_defconfig";
   version = "6.15.7-q6a";
   modDirVersion = "6.15.7";
   src = inputs.kernel-q6a;
+  
+  structuredExtraConfig = with lib.kernel; {
+    EFI_ZBOOT = lib.mkForce no;
+    NVME_AUTH = lib.mkForce yes;
+  };
+
+  extraConfig = ''
+    NOVA_CORE n
+    COMPRESSED_INSTALL n
+  '';
+
+  kernelPatches = [];
+  ignoreConfigErrors = true;
 }
